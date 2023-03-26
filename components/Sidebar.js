6f569fd77c0f58775b0b63920774397423b5864c
Ragini Pandey <pandeyragini55@gmail.com>
2023-03-27 02:04:25 +0530

    Created new branch

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,12 @@
+import { HomeIcon } from "@heroicons/react/outline";
 function Sidebar() {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <button>
+        <HomeIcon className={"w-5 h-5"} />
+        Home
+      </button>
+    </div>
+  );
 }
 export default Sidebar;

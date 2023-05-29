fdf7d9f760032f3ccbbcac3f0498713cc179a7c2
Ragini Pandey <pandeyragini55@gmail.com>
2023-05-30 00:03:54 +0530

    Center Css and middlleware added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
+import Center from "../components/Center";
 import Sidebar from "../components/Sidebar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden">
-      <main>
+      <main className="flex">
         <Sidebar />
+        <Center />
       </main>
     </div>
   );
